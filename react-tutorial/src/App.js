b607c5d93aec27e50d3c972d2b078e7ad2e28c7d
Mariana Correa <mpcs270391@gmail.com>
2022-05-19 14:41:08 -0300

    Inclusão de títulos e nome da coluna da tabela.

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -26,10 +26,13 @@ class App extends Component {
 
         return (
             <div className="container">
+                <h1>React Tutorial</h1>
+                <p> Add a character with a name and a job to the table.</p>
                 <Table
                     characterData={characters}
                     removeCharacter={this.removeCharacter}
                 />
+                <h2>Add New</h2>
                 <Form handleSubmit={this.handleSubmit} />
             </div>
         );
